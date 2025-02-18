236b50b506bc7406a941cb8bc33f6acac06e1d23
matt <matt@claritum.com>
2025-02-18 18:55:04 +0000

    Remove types no longer supported in v4. See https://github.com/apache/tinkerpop/blob/master/docs/src/upgrade/release-4.x.x.asciidoc#changes-to-serialization-1

--- src/Driver/MimeType.php
+++ src/Driver/MimeType.php
@@ -6,9 +6,10 @@ namespace Kynx\Gremlin\Driver;
 
 enum MimeType: string
 {
-    public const MimeType DEFAULT = self::GraphBinary;
+    public const MimeType DEFAULT = self::GraphBinary40;
 
-    case GraphSON2   = 'application/vnd.gremlin-v2.0+json';
-    case GraphSON3   = 'application/vnd.gremlin-v3.0+json';
-    case GraphBinary = 'application/vnd.graphbinary-v1.0';
+    case GraphSON2     = 'application/vnd.gremlin-v2.0+json';
+    case GraphSON3     = 'application/vnd.gremlin-v3.0+json';
+    case GraphBinary10 = 'application/vnd.graphbinary-v1.0';
+    case GraphBinary40 = 'application/vnd.graphbinary-v4.0';
 }
